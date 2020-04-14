cdd7fa31dc4bf2f7db82050d8b2894beb67da288
Vítězslav Dvořák <info@vitexsoftware.cz>
2020-04-15 01:13:58 +0200

    Mainpage Menu now use card header

--- src/Ease/TWB4/Widgets/MainPageMenu.php
+++ src/Ease/TWB4/Widgets/MainPageMenu.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * EasePHPbricks - Mainpage Large Icons Menu
  *
@@ -9,8 +10,8 @@
 
 namespace Ease\TWB4\Widgets;
 
-class MainPageMenu extends \Ease\TWB4\Row
-{
+class MainPageMenu extends \Ease\TWB4\Row {
+
     /**
      * Sem se přidávají položky.
      *
@@ -21,14 +22,13 @@ class MainPageMenu extends \Ease\TWB4\Row
     /**
      * MainPage Menu
      */
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct(
-            null, null,
-            [
-                'class' => 'container',
-                'style' => 'margin: auto;',
-            ]
+                null, null,
+                [
+                    'class' => 'container',
+                    'style' => 'margin: auto;',
+                ]
         );
     }
 
@@ -39,18 +39,19 @@ class MainPageMenu extends \Ease\TWB4\Row
      * @param string $url   image link href url
      * @param string $image url
      * @param string $decription additional text
+     * @param array  $properties for Card
      * 
      * @return \Ease\Html\ATag
-     */ public function addMenuItem($title, $url, $image, $description , $buttonText = null )
-    {
-         
-         $icon = new \Ease\Html\ImgTag( $image, $title, ['alt'=>$title,'class'=>'card-img-top']);
-         new \Ease\Html\H5Tag($title);
-         $cardBody = new \Ease\Html\DivTag( new \Ease\Html\H5Tag($title) ,['class'=>'card-body'] );
-         $cardBody->addItem( new \Ease\Html\PTag( $description ,['class'=>'card-text'] ) );
-         $cardBody->addItem( new \Ease\TWB4\LinkButton($url, empty($buttonText) ? _('Visit') : $buttonText, 'primary' ));
-         $menuCard = new \Ease\TWB4\Card([ $icon, $cardBody ]);
-         
+     */
+    public function addMenuItem($title, $url, $image, $description, $buttonText = null, $properties = []) {
+
+        $icon = new \Ease\Html\ImgTag($image, $title, ['alt' => $title, 'class' => 'card-img-top']);
+        $cardHeader = new \Ease\Html\DivTag($title, ['class' => 'card-header']);
+        $cardBody = new \Ease\Html\DivTag(new \Ease\Html\H5Tag($title), ['class' => 'card-body']);
+        $cardBody->addItem(new \Ease\Html\PTag($description, ['class' => 'card-text']));
+        $cardBody->addItem(new \Ease\TWB4\LinkButton($url, empty($buttonText) ? _('Visit') : $buttonText, 'primary'));
+        $menuCard = new \Ease\TWB4\Card([$cardHeader, $icon, $cardBody], $properties);
+
         return $this->addItem(new \Ease\TWB4\Col(3, $menuCard));
     }
 
