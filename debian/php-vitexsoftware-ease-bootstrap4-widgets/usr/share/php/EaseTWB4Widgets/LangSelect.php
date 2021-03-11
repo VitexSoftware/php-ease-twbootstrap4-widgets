41a8d5d74301e5c5e5186cbb909e7dd95ab8e081
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-03-11 16:09:53 +0100

    Release v1.1

--- debian/php-vitexsoftware-ease-bootstrap4-widgets/usr/share/php/EaseTWB4Widgets/LangSelect.php
+++ debian/php-vitexsoftware-ease-bootstrap4-widgets/usr/share/php/EaseTWB4Widgets/LangSelect.php
@@ -1,8 +1,11 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
+/**
+ * EasePHP Twitter Bootstrap4  - Availble Languages Select
+ *
+ * @link       https://fontawesome.com/
+ * @author     Vítězslav Dvořák <vitex@arachne.cz>
+ * @copyright  2020 Vitex Software
  */
 
 namespace Ease\TWB4\Widgets;
@@ -12,20 +15,10 @@ namespace Ease\TWB4\Widgets;
  *
  * @author vitex
  */
-class LangLinks extends \Ease\Html\UlTag
-{
+class LangSelect extends \Ease\Html\SelectTag {
 
-    public function __construct($properties = [])
-    {
-        parent::__construct(null, $properties);
-        foreach (\Ease\Locale::singleton()->availble() as $code => $name) {
-            $name = substr($name, 0, strpos($name,' ('));
-            if (\Ease\Locale::$localeUsed == $code) {
-                $this->addItemSmart(new \Ease\Html\StrongTag(new \Ease\Html\ATag('?locale='.$code,
-                    $name)));
-            } else {
-                $this->addItemSmart(new \Ease\Html\ATag('?locale='.$code, $name));
-            }
-        }
+    public function __construct(string $name = 'locale', array $properties = []) {
+        parent::__construct($name, \Ease\Locale::singleton()->availble(), \Ease\Locale::$localeUsed, $properties);
     }
+
 }
